bbdbb2dc9420378ce1dceb395b9d744c767ad03d
gaoyutong <gaoyutong1016@gmail.com>
2022-06-20 14:36:54 -0700

    update friend

--- src/app/Jueji/juejiGenerator.ts
+++ src/app/Jueji/juejiGenerator.ts
@@ -14,9 +14,9 @@ export class JuejiGenerator{
       let jueji = new Jueji();
       jueji.name = name;
       jueji.availability = Math.random() < 0.5;
-      jueji.substitution = Math.random() < 0.3;
+      jueji.substitution = jueji.friends.length != 0 && Math.random() < 0.3;
       jueji.friends = (friendList as any)[name];
-      jueji.friend = jueji.friends[Math.floor(Math.random() * jueji.friends.length)];
+      jueji.friend = jueji.friends.length == 0 ? '' : jueji.friends[Math.floor(Math.random() * jueji.friends.length)];
       jueji.event = new EventList();
       jueji.event.chat = (chatList as any)[name];
       jueji.event.sex = (sexList as any)[name];
